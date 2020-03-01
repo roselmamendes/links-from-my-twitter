9eee4a0a3e0da10ac1c7bed0dadd54d60a725231
Roselma <roselma.mendes@gmail.com>
2020-03-01 13:01:58 -0300

    Extract metatags from an url

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from app.bookmark_service import generate_bookmark_from_twitter
 from jinja2 import Environment, FileSystemLoader
+from bs4 import BeautifulSoup
+import requests
 
 # 30, '20200210', '20200215'
 def print_bookmark_from_twitter(max_results, fromDate, toDate):
@@ -28,3 +30,22 @@ def _generate_html_with(date_range, bookmark_from_twitter):
     # to save the results
     with open(f'{date_range}_bookmarks.html', "w") as html_file:
         html_file.write(output_from_parsed_template)
+
+
+def extract_metatags_from(url):
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+
+    metas = soup.find_all('meta')
+    print("metas: " + str(metas))
+    url_image = None
+    url_title = None
+    for meta in metas:
+        if 'property' in meta.attrs:
+            if meta.attrs['property'] == 'og:image':
+                url_image = meta.attrs['content']
+
+            if meta.attrs['property'] == 'og:title':
+                url_title = meta.attrs['content']
+
+    print(f'Titulo: {url_title}')
+    print(f'Imagem: {url_image}')
