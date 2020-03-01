96e96986f0a5c74aa8df2ee74aaa92ae8b4c7bd4
Roselma <roselma.mendes@gmail.com>
2020-03-01 12:28:40 -0300

    Generate a html with bookmarks from twitter

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from app.bookmark_service import generate_bookmark_from_twitter
+from jinja2 import Environment, FileSystemLoader
 
 # 30, '20200210', '20200215'
 def print_bookmark_from_twitter(max_results, fromDate, toDate):
@@ -12,8 +13,18 @@ def print_bookmark_from_twitter(max_results, fromDate, toDate):
 def create_html_bookmark_from_twitter(max_results, fromDate, toDate):
     bookmark_from_twitter = generate_bookmark_from_twitter(max_results, fromDate, toDate)
 
-    _generate_html_with(bookmark_from_twitter)
+    _generate_html_with(f'{fromDate}_{toDate}', bookmark_from_twitter)
 
 
-def _generate_html_with(bookmark_from_twitter):
-    pass
+def _generate_html_with(date_range, bookmark_from_twitter):
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template(f'twitter_bookmark.html')
+    output_from_parsed_template = template.render(
+        date_range=date_range,
+        bookmarks=bookmark_from_twitter
+    )
+    print(output_from_parsed_template)
+
+    # to save the results
+    with open(f'{date_range}_bookmarks.html', "w") as html_file:
+        html_file.write(output_from_parsed_template)
